a5d99d84eb3b9a677d7520e26d87681f12183bb6
vincor1986 <vincent.coraldean@gmail.com>
2022-03-27 23:46:29 +0100

    added delete account route

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,8 +5,12 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("../../middleware/auth");
 
 const User = require("../../models/User");
+const getAuthUserId = require("../../utils/getAuthUserId");
+const Profile = require("../../models/Profile");
+const Publication = require("../../models/Publication");
 
 // @route   POST api/users
 // @desc    Register user
@@ -76,5 +80,34 @@ router.post(
     }
   }
 );
+
+//@Route    DELETE api/users/deleteaccount
+//@Desc     Delete account
+//@Access   Private
+router.delete("/deleteaccount", auth, async (req, res) => {
+  try {
+    const authUserId = getAuthUserId(req);
+    const user = await User.findById(authUserId);
+    const profile = await Profile.find({ user: authUserId });
+
+    for (let i = 0; i < user.pub_id_array.length; i++) {
+      const pubIndex = user.pub_id_array[i];
+      const publication = await Publication.findById(pubIndex);
+      const arrayIndex = publication.connectedUsers.indexOf(authUserId);
+      publication.connectedUsers.splice(arrayIndex, 1);
+      publication.coauthors.push(`${user.first_name} ${user.last_name}`);
+      await publication.save();
+    }
+    if (profile) {
+      await Profile.findByIdAndDelete(profile._id);
+    }
+    await User.findByIdAndDelete(authUserId);
+
+    return res.json({ msg: "Account deleted" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
